31603516b27491c4d823fe7bdf41ee1dc29a36fd
Gabriel Lima <gb.gl@hotmail.com>
2023-07-02 16:06:51 -0300

    changed db values

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
+/* eslint-disable indent */
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-  user: 'postgres',
-  password: 'senha',
-  host: 'localhost',
-  port: 5432,
-  database: 'launchstore'
+    user: 'postgres',
+    password: '12345',
+    host: 'localhost',
+    port: '5433',
+    database: 'PostgreSQL 15',
 });
